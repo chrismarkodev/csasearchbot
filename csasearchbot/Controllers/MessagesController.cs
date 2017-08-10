a4dd000672006b016fad4d2f8d6795344d9ef81e
Chris M <chrismarkodev@outlook.com>
2017-08-10 15:17:07 -0400

    incorporated the functionality of RollerSkillBot but without registering for the speach component.

--- csasearchbot/Controllers/MessagesController.cs
+++ csasearchbot/Controllers/MessagesController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using csasearchbot.Dialogs;
 
 namespace csasearchbot
 {
@@ -16,45 +17,11 @@ namespace csasearchbot
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
-            {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
-            }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
+            await Conversation.SendAsync(activity, () => new RootDispatchDialog());
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
+
             return response;
-        }
-
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
-            return null;
         }
     }
 }
---
+++ csasearchbot/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace csasearchbot
+{
+    public static class Utils
+    {
+        public const string GameDataKey = "GameData";
+
+        public static string RandomPick(string listText)
+        {
+            var list = listText.Split('|');
+            var random = new Random();
+            return list[random.Next(list.Length)];
+        }
+    }
+}
